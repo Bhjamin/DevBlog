11a2e8a2df0849efeb5d1dfa202bf9e4230c8ad7
Rico <richardl@hancock1.com>
2023-04-22 15:11:55 -0600

    website/components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import type { PropsWithChildren } from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const Layout: React.FC<PropsWithChildren & { fullWidth?: boolean }> = ({
+  fullWidth = false,
+  children,
+  ...props
+}) => {
+  return (
+    <>
+      <Nav />
+      <main
+        className={`mx-auto min-h-[calc(100vh-75px)] w-full ${
+          fullWidth ? "max-w-[1440px]" : "max-w-full"
+        }`}
+        {...props}
+      >
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
