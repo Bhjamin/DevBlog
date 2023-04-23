f377fa2b58c78abb2afd00bcc807f6da23e1631a
Bhjamin <benjamin@hancock1.com>
2023-04-22 18:14:05 -0600

    updated homepage to look awesome

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 const Footer: React.FC = () => {
-  return <div className="min-h-[300px] w-full bg-slate-800">Footer</div>;
+  return <div className="min-h-[300px] w-full bg-primary z-20">
+
+    <p className="text-base-content text-2xl">links</p>
+    
+    </div>;
 };
 
 export default Footer;
